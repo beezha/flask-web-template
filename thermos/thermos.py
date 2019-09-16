4ff96108251cca2d23006ee1f338307ad92cb3e9
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-09-16 17:06:16 -0400

    HTML Templates with Jinja2

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Sample title", user="Maurice Ilagan")
 
 
 if __name__ == '__main__':
