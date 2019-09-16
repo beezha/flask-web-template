467f569f2833e8e064bb538bf4ae4692663f31ed
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-09-16 17:13:55 -0400

    Using url_for

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html', title="Sample title", user="Maurice Ilagan")
 
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
