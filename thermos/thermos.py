36bba2937521315267ea0fcfc7e18ceebd81fcfa
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-09-16 17:42:25 -0400

    Views and Forms

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from _datetime import datetime
+from logging import DEBUG
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+
+bookmarks = []
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url=url,
+        user="maurice",
+        date=datetime.utcnow()
+    ))
 
 
 @app.route('/')
@@ -9,8 +23,13 @@ def index():
     return render_template('index.html', title="Sample title", user="Maurice Ilagan")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == "POST":
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('store url: ' + url)
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 
