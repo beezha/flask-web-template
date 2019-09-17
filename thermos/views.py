29101d60f9a358fe5a68bb9bfbfb3515e78f1fab
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-09-17 07:59:22 -0400

    Editing Bookmarks

--- thermos/views.py
+++ thermos/views.py
@@ -24,12 +24,27 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        bm = Bookmark(user=current_user, url=url, description=description)
+        bm = Bookmark(user=current_user, url=url, description=description, tags=tags)
         db.session.add(bm)
         db.session.commit()
-        flash("Stored '{}'".format(description))
+        flash("Stored '{}'".format(bm.description))
         return redirect(url_for('index'))
-    return render_template('add.html', form=form)
+    return render_template('bookmark_form.html', form=form, title="Add a bookmark")
+
+
+@app.route('/edit/<int:bookmark_id>', methods=['GET', 'POST'])
+@login_required
+def edit_bookmark(bookmark_id):
+    bookmark = Bookmark.query.get_or_404(bookmark_id)
+    if current_user != bookmark.user:
+        abort(403)
+    form = BookmarkForm(obj=bookmark)
+    if form.validate_on_submit():
+        form.populate_obj(bookmark)
+        db.session.commit()
+        flash("Stored '{}'".format(bookmark.description))
+        return redirect(url_for('user', username=current_user.username))
+    return render_template('bookmark_form.html', form=form, title="Edit bookmark")
 
 
 @app.route('/user/<username>')
