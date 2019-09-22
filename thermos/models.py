4ed4d2266ea09ec65e98c611bdbe30cced74953d
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-09-22 10:01:56 -0400

    Add tags to migration

--- thermos/models.py
+++ thermos/models.py
@@ -6,6 +6,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from thermos import db
 
+tags = db.Table('bookmark_tag',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+                db.Column('bookmark_id', db.Integer, db.ForeignKey('bookmark.id'))
+                )
+
 
 class Bookmark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +18,8 @@ class Bookmark(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.String(300))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    _tags = db.relationship('Tag', secondary=tags,
+                            backref=db.backref('bookmarks', lazy='dynamic'))
 
     @staticmethod
     def newest(num):
@@ -46,3 +53,11 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False, unique=True, index=True)
+
+    def __repr__(self):
+        return self.name
